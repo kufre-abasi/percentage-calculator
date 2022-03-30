53dd75d5ee6945c619b39c807274d29875edb1b7
Kufre-abasi <kufreabasibassey3@gmail.com>
2022-03-30 18:52:55 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+function calc() {
+    var amount = Number(`5000`)
+    var bill = Number(document.getElementById('baseAmount').value);
+    if( bill = amount ){
+      var loadpercent = Number(document.getElementById('loanBill').value);
+      var load = bill *(loadpercent/100);
+       }
+          else{
+            alert('invalid')
+          };
+        if(bill = amount){
+          var conpercent = Number(document.getElementById('convertibleBill').value);
+          var con = bill *(conpercent/100)
+        } 
+        else{
+          alert('invalid')
+        };
+        if(bill = amount){
+          var profitpercent = Number(document.getElementById('convertibleBill').value);
+          var profit = bill *(profitpercent/100)
+        } 
+        else{
+          alert('invalid')
+        };
+        if(bill = amount){
+          var equitypercent = Number(document.getElementById('convertibleBill').value);
+          var equity = bill *(equitypercent/100)
+        } 
+        else{
+          alert('invalid')
+        };
+        if(bill = amount){
+          var grantpercent = Number(document.getElementById('convertibleBill').value);
+          var grant = bill *(grantpercent/100)
+        } 
+        else{
+          alert('invalid')
+        };
+        if( loadpercent + conpercent + profitpercent + equitypercent + grantpercent = '100'){
+
+        }
+
+
+    document.getElementById("showload").innerHTML = "$" + Number(load).toFixed(2);
+    document.getElementById("showcon").innerHTML = "$" + Number(con).toFixed(2);
+    document.getElementById("showprofit").innerHTML = "$" + Number(profit).toFixed(2);
+    document.getElementById("showequity").innerHTML = "$" + Number(equity).toFixed(2);
+    document.getElementById("showgrant").innerHTML = "$" + Number(grant).toFixed(2);
+  }
+
